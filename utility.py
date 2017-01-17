092210d8d94d4315bd2a690451ac01b2ad7b30d8
Sean Klei <stk1151@rit.edu>
2017-01-17 09:58:37 -0500

    .json is a function not an attribute

--- utility.py
+++ utility.py
@@ -40,7 +40,7 @@ def getMembers():
 	}
 
 	r = requests.get(API_PATH + '/groups', data=values)
-	print(r.json)
+	print(r.json())
 
 def invalidSearch():
     with open('failed_search.txt') as sayings:
