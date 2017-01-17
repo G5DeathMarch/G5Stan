b636908453845134658886adafeffa6d845825fb
Sean Klei <stk1151@rit.edu>
2017-01-17 11:22:17 -0500

    I keep forgetting that strings don't have .append

--- commands.py
+++ commands.py
@@ -89,8 +89,8 @@ def atGroup(group_message):
 	send them a message that should give them a notification
 	"""
 	members = getMembers()
-	bot_message = 'Current Members:'
+	bot_message = ['Current Members:']
 	for member in members:
 		bot_message.append(member.nickname)
-	bot_message(bot_message)
+	bot_message(''.join(bot_message))
 		
