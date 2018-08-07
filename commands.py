16cfec6b29acd9ad6d3a38c0b508a8f4934f7db2
Sean Klei <sklei1151@gmail.com>
2018-08-07 07:22:10 -0700

    Finished Crell implementation and Fixed up some Stan bugs (#6)
    * converting index.py to use spaces for indentation instead of a mix
    
    * adding print statement
    
    * hopeful
    
    * adding print statements and also converting tabas to spaces
    
    * more prints, added .gitignore to ignore pycache, and removed some .swp file?
    
    * importing random
    
    * adding comment and commenting out crellpic
    
    * fixing url
    
    * removing prints
    
    * adding print and fixing indentation
    
    * adding text?
    
    * adding a print statement back and updating a debug statement
    
    * removing newlines from image_links (so we don't accidentally get no image
    
    * no text?
    
    * put everything in double quotes and see if we can't get the imageurl in double quotes too
    
    * WE NEED TO STRIP OUR URLS INCASE THEY HAVE ANY EXTRA SPACES
    
    * let's have it so that we don't strip our newlines in the normal message (incase we do want to send new lines), but we'll sure as hell strip it in the invalid search
    
    * better start throwing bills, cause I'm stripping hard! (also removed the shift+enter in the README.md, but that isn't nearly as fun)
    
    * readding print statement
    
    * do quotes and a join matter?
    
    * send json?
    
    * let's also send json here too. Since by using data we use the default content type (which may not be application/json)
    
    * adding these print statements for debugging purposes
    
    * trying to remove this pics folder
    
    * Removing compiled python files and adding *.pyc into source control
    
    - removing a pyc file. why is a compiled file in here??
    - let's also ignore all compiled python files too
    - let's also remove this .pyc file
    
    * removing unneeded print statement

--- commands.py
+++ commands.py
@@ -2,81 +2,82 @@ import requests
 import sys
 import os
 import random
-from utility import botMessage, invalidSearch, obtainHotSubmissions
+from utility import botMessage, botImageMessage, invalidSearch, obtainHotSubmissions
 
 GIF_LIMIT = 1
 
 def getImage(searchTerm):
-	"""
-	Stan searches the GIFY API for a gif that matches the search 
-	term.
-	"""
-	url = "http://api.giphy.com/v1/gifs/search"
-	key = os.environ.get('GIPHY_KEY')
-	payload = {
-		'q':searchTerm,
-		'limit': GIF_LIMIT,
-		'api_key':key
-	}
-	try:
-		request = requests.get(url, params=payload)
-		# lets grab a random gif from the returned images.
-		gif = random.choice(request.json()['data'])
-		message = gif['images']['downsized']['url']
-		botMessage(message)
-	except:
-		# If we failed to find something from our search,
-		# we send back an error message. Stan style.
-		invalidSearch()
-	
+    """
+    Stan searches the GIFY API for a gif that matches the search 
+    term.
+    """
+    url = "http://api.giphy.com/v1/gifs/search"
+    key = os.environ.get('GIPHY_KEY')
+    payload = {
+        'q':searchTerm,
+        'limit': GIF_LIMIT,
+        'api_key':key
+    }
+    try:
+        request = requests.get(url, params=payload)
+        # lets grab a random gif from the returned images.
+        gif = random.choice(request.json()['data'])
+        message = gif['images']['downsized']['url']
+        botMessage(message)
+    except:
+        # If we failed to find something from our search,
+        # we send back an error message. Stan style.
+        invalidSearch()
+    
 def cheerUp():
-	"""
-	Stan sends a message that will cheer up the members of the group
-	chat.
-	"""
-	with open('compliments.txt') as compliments:
-		message = random.choice(compliments.readlines())
-		botMessage(message)
+    """
+    Stan sends a message that will cheer up the members of the group
+    chat.
+    """
+    with open('compliments.txt') as compliments:
+        message = random.choice(compliments.readlines()).strip()
+        botMessage(message)
 
 def helpMeStan():
-	"""
-	Will detail what stan can do and will use the README
-	to grab all the information about the different functions
-	"""
-	function_lines = []
-	with open('README.md') as readme:
-		for line in readme:
-			# We're checking to see if we hit the string that
-			# shows when we're talking about commands
-			if 'commands:' in line:
-				for line in readme:
-					"""
-					 if we ever get to the point where commands
-					 aren't last in the README, this is where
-					 we'd break.
-					"""
-					function_lines.append(line)
-		
-		message = ''.join(function_lines)
-		botMessage(message)
+    """
+    Will detail what stan can do and will use the README
+    to grab all the information about the different functions
+    """
+    function_lines = []
+    with open('README.md') as readme:
+        for line in readme:
+            # We're checking to see if we hit the string that
+            # shows when we're talking about commands
+            if 'commands:' in line:
+                for line in readme:
+                    """
+                     if we ever get to the point where commands
+                     aren't last in the README, this is where
+                     we'd break.
+                    """
+                    function_lines.append(line)
+        
+        message = ''.join(function_lines)
+        botMessage(message)
 
 def eyeBleach():
-	"""
-	Will send 3 gifs/images that will be of adorable things that
-	should cover up the current conversation screen. We'll just
-	scrape from r/eyebleach
-	"""
-	# we're going to grab the top five incase one of the submissions
-	# is a reddit text post, then we can filter it out.
-	submissions = obtainHotSubmissions('eyebleach', num_of_sub=5)
+    """
+    Will send 3 gifs/images that will be of adorable things that
+    should cover up the current conversation screen. We'll just
+    scrape from r/eyebleach
+    """
+    # we're going to grab the top five incase one of the submissions
+    # is a reddit text post, then we can filter it out.
+    submissions = obtainHotSubmissions('eyebleach', num_of_sub=5)
 
-	sub_count = 0
+    sub_count = 0
 
-	for submission in submissions:
-		if 'reddit.com' not in submission.url and sub_count < 3:
-			botMessage(submission.url)
-			sub_count += 1
+    for submission in submissions:
+        if 'reddit.com' not in submission.url and sub_count < 3:
+            botMessage(submission.url)
+            sub_count += 1
 
-#def atGroup():
-
-	
+def crellPic():
+    with open('image_links.txt') as pics:
+        image = random.choice(pics.readlines())
+        botImageMessage(image) 
--- utility.py
+++ utility.py
@@ -1,37 +1,51 @@
-import requests, sys, os, praw
+import requests, sys, os, praw, random
 
 #sends a standard message to the group
+BASE_URL = 'https://api.groupme.com/v3/bots/post'
+
 def botMessage(message):
-	#retrieve the appropriate bot_id from the JSON
-	bot_id = os.environ.get('BOT_ID')
-	values = {
-		'bot_id' : bot_id,
-		'text' : str(message),
-	}
-	r = requests.post('https://api.groupme.com/v3/bots/post', data = values)
+    bot_id = os.environ.get('BOT_ID')
+    values = {
+        'bot_id' : bot_id,
+        'text' : str(message),
+    }
+    print("send message values: {}".format(values))
+    r = requests.post(BASE_URL, json = values)
+
+def botImageMessage(image_url):
+    bot_id = os.environ.get('BOT_ID')
+    values = {
+            'bot_id' : bot_id,
+            'attachments' : [{
+                'type' : 'image',
+                'url' : image_url.strip()
+            }]
+    }
+    print("send image values: {}".format(values))
+    r = requests.post(BASE_URL, json = values)
 
 def invalidSearch():
-	with open('failed_search.txt') as sayings:
-		message = random.choice(sayings.readlines())
-		botMessage(message)
+    with open('failed_search.txt') as sayings:
+        message = random.choice(sayings.readlines()).strip()
+        botMessage(message)
 
 def obtainHotSubmissions(subreddit_name, num_of_sub=1):
-	"""
-	Will take a subreddit string, and will grab a 
-	number of urls and return them. The subreddit 
-	string doesn't need the r/, but we'll make sure to 
-	get rid of it before hand.
-	"""
-	# remove the r/ from the subreddit name
-	if subreddit_name.startswith('r/'):
-		subreddit_name = subreddit_name[2:]
+    """
+    Will take a subreddit string, and will grab a 
+    number of urls and return them. The subreddit 
+    string doesn't need the r/, but we'll make sure to 
+    get rid of it before hand.
+    """
+    # remove the r/ from the subreddit name
+    if subreddit_name.startswith('r/'):
+        subreddit_name = subreddit_name[2:]
 
-	reddit = praw.Reddit(client_id=os.environ.get('REDDIT_CLIENT_ID'),
-						 client_secret=os.environ.get('REDDIT_CLIENT_SECRET'),
-						 user_agent=os.environ.get('USER_AGENT'),
-						 username=os.environ.get('REDDIT_USERNAME'),
-						 password=os.environ.get('REDDIT_PASSWORD'))
+    reddit = praw.Reddit(client_id=os.environ.get('REDDIT_CLIENT_ID'),
+                         client_secret=os.environ.get('REDDIT_CLIENT_SECRET'),
+                         user_agent=os.environ.get('USER_AGENT'),
+                         username=os.environ.get('REDDIT_USERNAME'),
+                         password=os.environ.get('REDDIT_PASSWORD'))
 
-	subreddit = reddit.subreddit(subreddit_name)
+    subreddit = reddit.subreddit(subreddit_name)
 
-	return subreddit.hot(limit=num_of_sub)
+    return subreddit.hot(limit=num_of_sub)
