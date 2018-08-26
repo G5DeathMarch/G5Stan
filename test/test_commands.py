4b2bfd12a40186bae23578c9a369408e2bae3411
Sean Klei <sklei1151@gmail.com>
2018-08-25 19:43:38 -0700

    added tests for get_image

--- test/test_commands.py
+++ test/test_commands.py
@@ -57,29 +57,42 @@ class TestGetImage(object):
         random_choice.return_value = returned_gif
 
         # Call test function
-        with mock.patch('utility.bot_message'):
-            commands.get_image(self.search_term, self.num_of_gifs)
+        commands.get_image(self.search_term, self.num_of_gifs)
 
         # Verify Mocks
+        """
+        We use the has_calls with any_order=True because the system could
+        call the mock multiple times. So we can't just have a called_once_with
+        because it will fail.
+        """
         environ_get.assert_has_calls([call('GIPHY_KEY')], any_order=True)
         requests_get.assert_called_once_with(self.url, params=self.expected_payload)
         random_choice.assert_called_once_with(json_data)
         mock_bot_message.assert_called_once_with(returned_url)
 
+    @mock.patch('commands.invalid_search')
     @mock.patch.object(requests, 'get')
     @mock.patch.object(os.environ, 'get')
-    def test_get_image_requests_throw_exception(self, environ_get, requests_get,):
+    def test_get_image_requests_throw_exception(self, environ_get, requests_get, mock_invalid_search):
+        environ_get.side_effect = os_get_side_effect
+        requests_get.side_effect = Mock(side_effect=requests.RequestException)
 
-        pass
+        # Call test function
+        commands.get_image(self.search_term, self.num_of_gifs)
+
+        # Verify Mocks
+        environ_get.assert_has_calls([call('GIPHY_KEY')], any_order=True)
+        requests_get.assert_called_once_with(self.url, params=self.expected_payload)
+        mock_invalid_search.assert_called_once()
 
     @mock.patch.object(os.environ, 'get')
     def test_get_image_no_environment_key(self, environ_get):
         # Setup Mock
-        environ_get.side_effect = Mock(side_effect=requests.RequestException('Test'))
+        environ_get.side_effect = Mock(side_effect=KeyError)
 
         # Call test function
-
-        pass
+        with pytest.raises(KeyError):
+            commands.get_image(self.search_term, self.num_of_gifs)
 
 
 class TestCheerUp(object):
